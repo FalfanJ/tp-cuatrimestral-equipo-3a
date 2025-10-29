c076b44bb24541efad4379e7ce1d340e213947a8
FalfanJ <falfan.jos@hotmail.com>
2025-10-29 06:32:52 -0300

    Avance TPC

---
+++ Dominio/Productos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Productos
+    {
+        public int IdProducto { get; set; }
+        public int Precio { get; set; }
+        public int Stock { get; set; }
+        public int StockMinimo { get; set; }
+        public string Descripcion { get; set; }
+        public Marcas Marca { get; set; }
+        public Categorias Categoria { get; set; }
+        public List<Proveedores> Proveedor { get; set; } = new List<Proveedores>();
+    }
+}
---
+++ Dominio/Ventas.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Ventas
+    {
+        public int IdVenta { get; set; }
+        public Productos Producto { get; set; }
+        public Clientes Cliente { get; set; }
+        public int CantidadVendida { get; set; }
+    }
+}
